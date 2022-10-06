1e3795036928003d1f398dad15b31612c27b54e6
Roby Jacob <robyjacob@ip-192-168-1-6.ap-south-1.compute.internal>
2022-10-06 15:18:04 +0530

    remove redundant test case and code, change input/output path

--- src/main/scala/VaccineAnalysis/Main.scala
+++ src/main/scala/VaccineAnalysis/Main.scala
@@ -1,9 +1,18 @@
 package VaccineAnalysis
 
+import org.apache.spark.sql.DataFrame
+
 
 object Main extends App {
-  val analysis: Analysis = new Analysis();
+  val basePath: String = "input";
 
-//  analysis.vaccineCount.show();
-  analysis.usDf.printSchema();
+  val analysis: Analysis = new Analysis(basePath);
+
+  val resDf: DataFrame = analysis.run;
+
+  resDf.write
+    .format("csv")
+    .mode("overwrite")
+    .save("output")
 }
+
--- src/test/scala/AnalysisTest.scala
+++ src/test/scala/AnalysisTest.scala
@@ -39,10 +39,10 @@ class AnalysisTest extends AnyFunSuite{
     StructField("Country", StringType, false)
   ));
 
-  val crctBasePath: String = "/Users/robyjacob/Downloads/Incubyte DE Assessment/SampleInputData";
+  val crctBasePath: String = "input";
 
   test("Analysis") {
-    val wrongBasePath: String = "/Users/robyjacob/Downloads/Incubyte DE Assessment/SampleInputDat";
+    val wrongBasePath: String = "../../../../input";
 
     assertThrows[FileNotFoundException] (new Analysis(wrongBasePath));
 
@@ -105,86 +105,5 @@ class AnalysisTest extends AnyFunSuite{
         |""".stripMargin).distinct();
 
     assert(analysis.run.collectAsList() == resDf.collectAsList());
-  }
-
-  test("Analysis.vaccineCount") {
-    val analysis : Analysis = new Analysis(crctBasePath);
-
-    val usDfData: List[Row] = List(
-      Row(1, "Roby", "MNO", "12282021", "USA"),
-      Row(2, "Rahul", "ABC", "06152022", "USA")
-    );
-
-    val indDfData: List[Row] = List(
-      Row(1, "Mehana", "1998-12-01", "LPO", "2022-01-01", "F", "IND")
-    );
-
-    val ausDfData: List[Row] = List(
-      Row(1, "Sameer", "MNO", "1952-08-13", "2022-02-20", "AUS"),
-      Row(2, "Vikas", "MNO", "1998-12-01", "2022-03-05", "AUS")
-    );
-
-    val usDF: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(usDfData), usDfSchema);
-
-    val indDf: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(indDfData), indDfSchema);
-
-    val ausDf: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(ausDfData), ausDfSchema);
-
-    analysis.usDf = usDF;
-    analysis.indDf = indDf;
-    analysis.ausDf = ausDf;
-
-    val resDf: DataFrame = usDF.select("Country", "VaccinationType")
-      .union(indDf.select("Country", "VaccinationType"))
-      .union(ausDf.select("Country", "VaccinationType"))
-      .groupBy("Country", "VaccinationType")
-      .agg(count(lit(1)).alias("VaccineCount"));
-
-    assert(analysis.vaccineCount.collectAsList() == resDf.collectAsList());
-  }
-
-  test("Analysis.vaccinatedPerc") {
-    val analysis : Analysis = new Analysis(crctBasePath);
-
-    val usDfData: List[Row] = List(
-      Row(1, "Roby", "MNO", "12282021", "USA"),
-      Row(2, "Rahul", "ABC", "06152022", "USA")
-    );
-
-    val indDfData: List[Row] = List(
-      Row(1, "Mehana", "1998-12-01", "LPO", "2022-01-01", "F", "IND")
-    );
-
-    val ausDfData: List[Row] = List(
-      Row(1, "Sameer", "MNO", "1952-08-13", "2022-02-20", "AUS"),
-      Row(2, "Vikas", "MNO", "1998-12-01", "2022-03-05", "AUS")
-    );
-
-    val usDF: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(usDfData), usDfSchema);
-
-    val indDf: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(indDfData), indDfSchema);
-
-    val ausDf: DataFrame = spark.createDataFrame(
-      spark.sparkContext.parallelize(ausDfData), ausDfSchema);
-
-    analysis.usDf = usDF;
-    analysis.indDf = indDf;
-    analysis.ausDf = ausDf;
-
-    val fullDf: DataFrame = usDF.select("Country")
-      .union(indDf.select("Country"))
-      .union(ausDf.select("Country"));
-
-    val totalPopulation: Long = fullDf.count();
-
-    val resDf: DataFrame = fullDf.groupBy("Country")
-      .agg((count(lit(1)) * 100 / totalPopulation).alias("PercentageVaccinated"));
-
-    assert(analysis.vaccinatedPerc.collectAsList() == resDf.collectAsList());
   }
 }
